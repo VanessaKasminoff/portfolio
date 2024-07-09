4897381178aa91eb4ecb76924411877a67a12ea6
Vanessa Kasminoff <sunnset07@gmail.com>
2024-07-08 19:21:00 -0700

    Initial react project setup and initial scss setup. Initial navbar creation and styling

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Navbar from "./components/Navbar"
+
+function App() {
+
+  return (
+    <div className="app">
+    <Navbar/>
+    <div className="hero-section">
+      <h1>Hello, welcome!</h1>
+    </div>
+    </div>
+  )
+}
+
+export default App
