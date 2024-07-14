d06a2520586b0a51af2580968f861257747c9ad1
Vanessa Kasminoff <sunnset07@gmail.com>
2024-07-13 20:09:24 -0700

    Created footer and modified landing page layout.

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,13 +1,25 @@
 import React from "react";
 import Navbar from "./Navbar";
+import Footer from "./Footer";
 
 const Landing = () => {
   return (
     <div className="app">
       <Navbar />
       <div className="hero-section">
-        <h1>Hello, world!</h1>
+        <div>
+          <h1>Hi! Thanks for stopping by! :)</h1>
+          <p>I'm Vanessa, a software developer</p>
+        </div>
+        <div className="hero-photo-div">
+          <img className="my-photo" src="/src/assets/images/me.jpg" />
+        </div>
       </div>
+      <Footer/>
+      {/* <div className="empty-div">
+        <h2>Lorem Ipsum</h2>
+        <button>I'm a button!</button>
+      </div> */}
     </div>
   );
 };
